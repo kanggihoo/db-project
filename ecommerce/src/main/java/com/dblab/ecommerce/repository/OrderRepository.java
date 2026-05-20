7dc485962f3dd8689810332759fe0121140db24a
kkh <11kkh19999@gmail.com>
2026-05-20 23:58:37 +0900

    feat: add phase 3 entity graph strategy

--- ecommerce/src/main/java/com/dblab/ecommerce/repository/OrderRepository.java
+++ ecommerce/src/main/java/com/dblab/ecommerce/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.dblab.ecommerce.repository;
 
 import com.dblab.ecommerce.entity.Orders;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -19,4 +20,11 @@ public interface OrderRepository extends JpaRepository<Orders, Long> {
             where o.userId = :userId
             """)
     List<Orders> findByUserIdWithFetchJoin(@Param("userId") Long userId);
+
+    @EntityGraph(attributePaths = {
+            "orderItems",
+            "orderItems.productSku",
+            "orderItems.productSku.product"
+    })
+    List<Orders> findGraphByUserId(Long userId);
 }
